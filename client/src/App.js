840f5d41ced35251020f569f3a8177f2dd6300a4
Dzeus2510 <vudnhe172175@fpt.edu.vn>
2023-09-08 13:39:55 +0700

    make more of Createpost

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/post" element={<Createpost />} />
+    <Route path="/display" element={<Createpost />} />
     </Routes>
   </Router>
   </div>
